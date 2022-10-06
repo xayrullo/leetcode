b90617b14e9dbf6ce45915bd8decfa47d03179d2
xayrullo <xayrullo.jaloldinov@gmail.com>
2022-10-06 11:44:05 +0500

    1480-running-sum optimized

--- 1480-running-sum.js
+++ 1480-running-sum.js
@@ -1,11 +1,17 @@
 var runningSum = function(nums) {
     let createdArr = [];
     for (let ind = 0; ind < nums.length; ind++) {
-        let sum = nums[ind];
-        for (let index = 0; index < ind; index++) {
-            sum = sum + nums[index]   
+        if (ind === 0) createdArr.push(nums[ind])
+        else {
+            console.log(createdArr[ind-1], nums[ind])
+            createdArr.push(createdArr[ind-1] + nums[ind])
         }
-        createdArr.push(sum)
+        // let sum = nums[ind];
+
+        // for (let index = 0; index < ind; index++) {
+        //     sum = sum + nums[index]   
+        // }
+        // createdArr.push(sum)
     }
     return createdArr;
 }
