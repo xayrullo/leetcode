1e0de5f12dc3ba368b12286bf269a83cdfe315fe
xjaloldinov <xjaloldinov@texnomart.uz>
2023-10-19 10:24:37 +0500

    Added TreeNode class

---
+++ classes/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var TreeNode = /** @class */ (function () {
+    function TreeNode(val, left, right) {
+        this.val = val === undefined ? 0 : val;
+        this.left = left === undefined ? null : left;
+        this.right = right === undefined ? null : right;
+    }
+    return TreeNode;
+}());
+exports.default = TreeNode;
